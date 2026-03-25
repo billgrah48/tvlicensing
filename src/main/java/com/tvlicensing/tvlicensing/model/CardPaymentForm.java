4894bde9a527ab0fbe9d9d9195bfd384f224f671
billgrah48 <billy_graham@hotmail.co.uk>
2026-03-25 15:40:36 +0000

    Accessibility improved css - font changed to match gov uk

--- src/main/java/com/tvlicensing/tvlicensing/model/CardPaymentForm.java
+++ src/main/java/com/tvlicensing/tvlicensing/model/CardPaymentForm.java
@@ -9,16 +9,13 @@ public class CardPaymentForm {
     @NotBlank(message = "Name on card is required")
     private String nameOnCard;
 
-    @NotBlank(message = "Card number is required")
     @CreditCardNumber(message = "Please enter a valid card number")
     private String cardNumber;
 
-    @NotBlank(message = "Expiry date is required")
-    @Pattern(regexp = "(0[1-9]|1[0-2])/\\d{2}", message = "Expiry date must be in MM/YY format")
+    @Pattern(regexp = "(0[1-9]|1[0-2])/\\d{2}", message = "Expiry date is required and must be in MM/YY format")
     private String expiryDate;
 
-    @NotBlank(message = "CVV is required")
-    @Pattern(regexp = "\\d{3}", message = "CVV must be 3 digits")
+    @Pattern(regexp = "\\d{3}", message = "CVV is required and must be 3 digits")
     private String cvv;
 
     public CardPaymentForm() {}
